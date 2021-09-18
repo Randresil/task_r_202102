67487a6921a9aa549fe456e42c92f22de23a931b
Randresil <ra.silva@uniandes.edu.co>
2021-09-18 16:26:13 -0500

    Progresos 18sep

--- task_1/source/code/script_A.R
+++ task_1/source/code/script_A.R
@@ -68,27 +68,50 @@ cultivo_xlsx_sintotal <- cultivo_xlsx[c(-9, -63, -69, -86, -99, -105, -122, -151
                                         -195, -200, -207, -212, -216, -221, -236, -272, -291, 
                                         -302, -333, -341, -348, -352, -353, -354, -355), ] # Remover a mano por indexacion negativa
 
-cultivo_xlsx_sinNA2 <- cultivo_xlsx %>% drop_na(MUNICIPIO) # Al borrar un NA de una variable se borra la fila/observacion completa
-cultivo_xlsx_sinNA2 <- cultivo_xlsx_sinNA2[c(-329), ] # BASE FINAL :)
+cultivo_final <- cultivo_xlsx %>% drop_na(MUNICIPIO) # Al borrar un NA de una variable se borra la fila/observacion completa
+cultivo_final <- cultivo_final[c(-329), ] # BASE FINAL :)
 
   # Codigo para convertir las variables/columnas en tipo character
-cultivo_xlsx_sinNA2$`2008` = as.character(cultivo_xlsx_sinNA2$`2008`) # Toca colocar con `` la variable
-cultivo_xlsx_sinNA2$`2009` = as.character(cultivo_xlsx_sinNA2$`2009`)
-cultivo_xlsx_sinNA2$`2010` = as.character(cultivo_xlsx_sinNA2$`2010`)
-cultivo_xlsx_sinNA2$`2011` = as.character(cultivo_xlsx_sinNA2$`2011`)
-cultivo_xlsx_sinNA2$`2012` = as.character(cultivo_xlsx_sinNA2$`2012`)
-cultivo_xlsx_sinNA2$`2013` = as.character(cultivo_xlsx_sinNA2$`2013`)
-cultivo_xlsx_sinNA2$`2014` = as.character(cultivo_xlsx_sinNA2$`2014`)
-cultivo_xlsx_sinNA2$`2015` = as.character(cultivo_xlsx_sinNA2$`2015`)
-cultivo_xlsx_sinNA2$`2016` = as.character(cultivo_xlsx_sinNA2$`2016`)
-cultivo_xlsx_sinNA2$`2017` = as.character(cultivo_xlsx_sinNA2$`2017`)
-cultivo_xlsx_sinNA2$`2018` = as.character(cultivo_xlsx_sinNA2$`2018`)
-cultivo_xlsx_sinNA2$`2019` = as.character(cultivo_xlsx_sinNA2$`2019`)
-glimpse(cultivo_xlsx_sinNA2)
+cultivo_final$`2008` = as.character(cultivo_final$`2008`) # Toca colocar con `` la variable
+cultivo_final$`2009` = as.character(cultivo_final$`2009`)
+cultivo_final$`2010` = as.character(cultivo_final$`2010`)
+cultivo_final$`2011` = as.character(cultivo_final$`2011`)
+cultivo_final$`2012` = as.character(cultivo_final$`2012`)
+cultivo_final$`2013` = as.character(cultivo_final$`2013`)
+cultivo_final$`2014` = as.character(cultivo_final$`2014`)
+cultivo_final$`2015` = as.character(cultivo_final$`2015`)
+cultivo_final$`2016` = as.character(cultivo_final$`2016`)
+cultivo_final$`2017` = as.character(cultivo_final$`2017`)
+cultivo_final$`2018` = as.character(cultivo_final$`2018`)
+cultivo_final$`2019` = as.character(cultivo_final$`2019`)
+glimpse(cultivo_final)
 
   # Codigo con funcion lapply para convertir (por medio de loops) el variables en chr
     # cultivo_xlsx_sinNA2 = lapply(data_xls,as.character) %>% data.frame()
 
-cultivo_xlsx_sinNA2 %>% pivot_longer(!CODDEPTO,!DEPARTAMENTO,!CODMPIO,!MUNICIPIO,names_to="year",values_to="cultivos") 
-cultivo_xlsx_sinNA2 = cultivo_xlsx_sinNA2 %>% pivot_longer(CODDEPTO:MUNICIPIO,names_to="year",values_to="cultivos") 
+cultivo_final %>% pivot_longer(!CODDEPTO,!DEPARTAMENTO,!CODMPIO,!MUNICIPIO,names_to="year",values_to="cultivos") 
+cultivo_final = cultivo_final %>% pivot_longer(CODDEPTO:MUNICIPIO,names_to="year",values_to="cultivos") 
+
+
+
+#------------------------------------------------------------------------------#
+#----------------- PUNTO 3 - Base de datos dataframe GEIH ---------------------#
+#------------------------------------------------------------------------------#
+rm(list = ls())
+
+cat("Instruccion 3.1: Importe las bases de datos Cabecera - Caracteristicas generales (Personas).rds, Cabecera - Ocupados.rds, Cabecera - Desocupados.rds, Cabecera - Inactivos.rds y Cabecera - Fuerza de trabajo.rds de la carpeta data/input/2019. Luego use las variables secuencia_p, orden y directorio para unir en una única base de datos. Mantenga en la base de datos únicamente las variables secuencia_p, orden, directorio, P6020, P6040, P6030S1, P6440, P6450, P6920, INGLABO, DPTO, fex_c_2011, ESC, MES, P6050 y las variables que usted va a generar para saber si una persona es ocupada, desocupada, inactiva y/o fuerza de trabajo.")
+
+setwd("/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019")
+list.files() # Listar los archivos que se encuentran en el directorio
+
+data_ccg = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Caracteristicas generales (Personas).rds")
+data_cdesoc = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Desocupados.rds")
+data_cfuerza = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102task_1/data/input/2019/Cabecera - Fuerza de trabajo.rds")
+data_cinact = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Inactivos.rds")
+data_cocup = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Ocupados.rds")
+
+str(data_ccg)
+colnames(data_ccg) 
+  # Variables de interes son: directorio secuencia_p orden
+  # Variables a mantener son: directorio secuencia_p orden P6020 P6040 P6030S1 P6440 P6450 P6920 INGLABO DPTO fex_c_2011 ESC MES P6050 y las demas que considere
 
