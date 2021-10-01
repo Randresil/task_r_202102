55c5abab3bf5b8899b8fa8f0c135502e2183668d
Randresil <ra.silva@uniandes.edu.co>
2021-10-01 12:40:35 -0500

    cambios 1 octubre

--- task_1/source/code/script_A.R
+++ task_1/source/code/script_A.R
@@ -1,12 +1,13 @@
 # Elaborado por: Ricardo Andres Silva Torres
 # Colaboradores: Ninguno (solo yo jajaja)
 # Fecha de elaboracion: 15/09/2021
-# Ultima modificacion: por definir /09/2021
+# Ultima modificacion: por definir XX/09/2021
 
 # configuracion inicial 
 rm(list = ls()) # limpia el entorno de R
 if(!require(pacman)) install.packages("pacman") ; require(pacman) # Instalar la librería pacman
 p_load(rio,readxl,haven,skimr,WriteXLS,tidyverse) # Llamar y/o instalar las librerías de la clase
+installed.packages() # Confirmar las librerias o paquetes instalados en el momento
 Sys.setlocale("LC_CTYPE", "en_US.UTF-8") # Encoding UTF-8
 
 #######################################
@@ -33,8 +34,6 @@ vector_par = vector_impar + 1
 rm(list = ls())
 
 cat("Importe la base de datos cultivos que se encuentra en la carpeta data/input, limpie la base de datos eliminando las observaciones que no tienen información relevante. Luego pivotee la base de datos para que quede en formato long.")
-
-installed.packages()
 ?rio::import
 
   # Opcion de importar con el paquete rio
@@ -60,7 +59,8 @@ object.size(cultivo_xlsx) # Peso del archivo en bytes
 
   # Para la lIMPIEZA del DATAFRAME
     # Para trabajar con las columnas con funcion SELECT() y derivados
-    # Para trabajar con las filas con funcion SUBSET()
+    # Para trabajar con las filas con funcion SUBSET() o tambien con filter()
+    # mutate() nos pemrite crear nuevas variables
 
 cultivo_xlsx_sinNA <- cultivo_xlsx %>% drop_na() # Ahora todas las observaciones tienen info y ningun NA
 cultivo_xlsx_sintotal <- cultivo_xlsx[c(-9, -63, -69, -86, -99, -105, -122, -151, -160, -188,
@@ -71,6 +71,14 @@ cultivo_final <- cultivo_xlsx %>% drop_na(MUNICIPIO) # Al borrar un NA de una va
 cultivo_final <- cultivo_final[c(-329), ] # BASE FINAL :)
 
   # Codigo para convertir las variables/columnas en tipo character
+  # Codigo con las variables de tipo char por medio de un loop
+    # years = 1999:2007 %>% as.character()
+      # for(var in years){
+        # data[,var] = as.numeric(data[,var])
+      # }
+
+  # Tambien se puede utilizar la funcion de as.numeric o as.character para transformar las columnas
+
 cultivo_final$`2008` = as.character(cultivo_final$`2008`) # Toca colocar con `` la variable
 cultivo_final$`2009` = as.character(cultivo_final$`2009`)
 cultivo_final$`2010` = as.character(cultivo_final$`2010`)
@@ -88,8 +96,11 @@ glimpse(cultivo_final)
   # Codigo con funcion lapply para convertir (por medio de loops) el variables en chr
     # cultivo_xlsx_sinNA2 = lapply(data_xls,as.character) %>% data.frame()
 
-cultivo_final %>% pivot_longer(!CODDEPTO,!DEPARTAMENTO,!CODMPIO,!MUNICIPIO,names_to="year",values_to="cultivos") 
-cultivo_final = cultivo_final %>% pivot_longer(CODDEPTO:MUNICIPIO,names_to="year",values_to="cultivos") 
+rm(cultivo_xlsx, cultivo_xlsx_sinNA, cultivo_xlsx_sintotal)
+view(cultivo_final)
+
+cultivo_final %>% pivot_longer(!CODDEPTO:MUNICIPIO, names_to="year", values_to="cultivos") 
+cultivo_long = cultivo_final %>% pivot_longer(!CODDEPTO:MUNICIPIO, names_to="year", values_to="cultivos") # base de cultivos final
 
 
 #------------------------------------------------------------------------------#
@@ -99,9 +110,10 @@ rm(list = ls())
 
 cat("Instruccion 3.1: Importe las bases de datos Cabecera - Caracteristicas generales (Personas).rds, Cabecera - Ocupados.rds, Cabecera - Desocupados.rds, Cabecera - Inactivos.rds y Cabecera - Fuerza de trabajo.rds de la carpeta data/input/2019. 
     Luego use las variables secuencia_p, orden y directorio para unir en una única base de datos. 
-    Mantenga en la base de datos únicamente las variables secuencia_p, orden, directorio, P6020, P6040, P6030S1, P6440, P6450, P6920, INGLABO, DPTO, fex_c_2011, ESC, MES, P6050 y las variables 
+    Mantenga en la base de datos únicamente las variables secuencia_p, orden, directorio, P6020, P6040, P6920, INGLABO, DPTO, fex_c_2011, ESC, MES, P6050 y las variables
     que usted va a generar para saber si una persona es ocupada, desocupada, inactiva y/o fuerza de trabajo.")
-cat("Variables a buscar: directorio, secuencia_p, orden, P6020, P6040, P6030S1, P6440, P6450, P6920, INGLABO, dpto, fex_c_2011, ESC, MES, P6050 y las demas que considere")
+
+cat("Variables a buscar: secuencia_p, orden, directorio, P6020, P6040, P6920, INGLABO, DPTO, fex_c_2011, ESC, MES, P6050 y las demas que considere")
 
 setwd("/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019")
 list.files() # Listar los archivos que se encuentran en el directorio
@@ -112,7 +124,7 @@ fuerza = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad d
 inact = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Inactivos.rds")
 ocup = import(file = "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Andes/TallerR - 2021-2/task_r_202102/task_1/data/input/2019/Cabecera - Ocupados.rds")
 
-str(cgen)
+glimpse(cgen)
 
 cgen %>% select_all(tolower) %>% colnames()
 desoc %>% select_all(tolower) %>% colnames()
@@ -120,11 +132,10 @@ fuerza %>% select_all(tolower) %>% colnames()
 inact %>% select_all(tolower) %>% colnames()
 ocup %>% select_all(tolower) %>% colnames()
   # Variables de interes son: directorio, secuencia_p, orden
-  # Variables a mantener son: directorio, secuencia_p, orden, P6020, P6040, P6030S1, P6440, P6450, P6920, INGLABO, dpto, fex_c_2011, ESC, MES, P6050 y las demas que considere
+  # Variables a mantener son: secuencia_p, orden, directorio, P6020, P6040, P6920, INGLABO, DPTO, fex_c_2011, ESC, MES, P6050 y las demas que considere
   # Esencial empezar con la base de cg
   # CARACTERISTICAS GENERALES: P6020 P6040 ESC MES P6050 DPTO fex_c_2011
-  # Ocupados: INGLABO P6920 
-  # Variables P6030S1, P6440, P6450 NO ESTAN!!!
+  # Ocupados: INGLABO P6920 dpto fex inglabo
 
 base_unica = left_join(x = cgen, y = desoc, by = c("directorio","secuencia_p","orden"), suffix = c("", ""))
 base_unica = left_join(x = base_unica, y = fuerza, by = c("directorio","secuencia_p","orden"), suffix = c("", ""))
