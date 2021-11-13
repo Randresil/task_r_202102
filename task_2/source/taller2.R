df266bd5a7aa0922dc7a08eff2e97516d88576b8
randresil <ra.silva@uniandes.edu.co>
2021-11-13 09:50:59 -0500

    No se que fue lo que paso con los anteriores errores en el punto 3, pero SALIO JAJAJAAJJJA

--- task_2/source/taller2.R
+++ task_2/source/taller2.R
@@ -1,9 +1,11 @@
 # Elaborado por: Ricardo Andres Silva Torres
 # Colaboradores: Ninguno (solo yo jajaja)
 # Fecha de elaboracion: 07/11/2021
-# Ultima modificacion: por definir XX/09/2021
+# Ultima modificacion: 13/11/2021
 # Version de R: 4.1.2
-# Taller 2 (A) de Taller de R: Estadística y Programación
+# Version de MacOS: 12.0.1 Monterrey
+# Taller 2 (A) de Taller de R: Estadística y Programación 
+# Aunque creo que termine haciendo el B por la categoria del punto 2 jaja
 
 
 # Configuracion inicial ---------------------------------------------------
@@ -19,7 +21,8 @@ basic_dir <- "/Users/ricardoandressilvatorres/OneDrive - Universidad de los Ande
 setwd(paste0(basic_dir,"task_2")) # Tener objetos y direcciones a lo STATA
 getwd()
 
-cat("1.0. Crear lista: Cree un objeto tipo lista vacío, llámelo chip.")
+print("1. Loops")
+cat("1.0.Crear lista: Cree un objeto tipo lista vacío, llámelo chip.")
 chip = list()
 
 cat("1.1. Importar datos: Use un loop para importar cada archivo .xlsx de data/imput en 
@@ -46,7 +49,9 @@ for (i in 1:4){
   print(files[[i]]) # Loop para observar las direcciones de todos los archivos a importar
 }
   
-files = lapply(2017:2020, function(x) list.files(paste0("data/imput/",x),full.names=T)) %>% unlist()
+files = lapply(2017:2020, 
+               function(x) 
+                 list.files(paste0("data/imput/",x),full.names=T)) %>% unlist()
 class(files) # Char
 
 # Loop de importacion en la lista chip
@@ -56,16 +61,19 @@ for (i in 1:length(files)){
 
 
 # Punto 2 -----------------------------------------------------------------
+print("2. Funciones")
 cat("Crear función: Cree una función que extraiga de un dataframe dentro de chip, el 
     valor PAGOS(Pesos) para la categoría EDUCACION. Asegúrese de extraer el código DANE 
     del municipio y el periodo de la información.")
 
 ?as.data.frame
+?data.frame
 ?subset
 ?select
 
 df1 = as.data.frame(chip[[1]])
 View(df1)
+rm(df1)
 # Podemos observar que el codigo Dane esta en nombre columna 1
 # Periodo de informacion esta en observacion 2 columna 1.
 # Valor PAGOS(Pesos) esta en fila 7 columna 8
@@ -75,8 +83,7 @@ View(df1)
 # Creacion de la funcion para extrae datos
 extractdata = function(i, list, criterio){
   
-  # Primero, crear un dataframe con funcion tibble(), hay problemas por data_frame() deprecated. 
-  # Toca tibble() o data.frame()
+  # Primero, crear un dataframe con funcion tibble() o data.frame(), problemas por data_frame() deprecated. 
   df = data.frame(Codigo = NA, Periodo = NA, Valor = NA)
   list_i = list[[i]]
   
@@ -89,15 +96,17 @@ extractdata = function(i, list, criterio){
   # Nombres a columnas de la lista
   colnames(list_i) = list_i[7,]
   # Valor segun el criterio
-  df$Valor =  list_i %>% subset(NOMBRE == criterio) %>% select(`PAGOS(Pesos)`)
+  df$Valor =  list_i %>% subset(NOMBRE == criterio) %>% select(`PAGOS(Pesos)`) # Value
   return(df)
 }
 
-extractdata(i = 1, list = chip, criterio = "EDUCACIÓN")
-
+extractdata(i = 80, list = chip, criterio = "EDUCACIÓN")
 
 # Punto 3 -----------------------------------------------------------------
+print("3. Familia Apply")
+cat("Aplique la función creada en el punto anterior a todos los elementos de la lista chip.")
 
-# final = lapply(2017:2020, extractdata() 
-
+lapply(1:length(chip),
+               function(x) 
+                 extractdata(i = x, list = chip, criterio = "EDUCACIÓN"))
 
